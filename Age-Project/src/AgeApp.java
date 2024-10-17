1d7b3658a15670425488fe84d3c0ffd8bfe8eea4
29sakshu <sakshis29091@gmail.com>
2024-10-17 19:12:47 +0300

    Initial commit

---
+++ Age-Project/src/AgeApp.java
@@ -0,0 +1,14 @@
+public class AgeApp {
+    
+    public static void main(String[] args) {
+        int age = 16; 
+        if (age > 0 && age < 18) {
+            System.out.println("You are underage.");
+        } else if (age >= 65) {
+            System.out.println("You are retired.");
+        } else {
+            System.out.println("You are an adult.");
+        }
+        System.out.println("Press space to exit.");
+    }
+}
