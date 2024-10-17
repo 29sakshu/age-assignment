b03a82fcfab63cf51932b6a3a80b3327673b6ca5
29sakshu <sakshis29091@gmail.com>
2024-10-17 19:24:22 +0300

    Retirement condition added

--- Age-Project/src/AgeApp.java
+++ Age-Project/src/AgeApp.java
@@ -1,9 +1,13 @@
 public class AgeApp {
-    
     public static void main(String[] args) {
-        int age = 16; 
+        int age = 67; 
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
+            if (age >= 15) {
+                System.out.println("You can drive a moped.");
+
+
+            }
         } else if (age >= 65) {
             System.out.println("You are retired.");
         } else {
